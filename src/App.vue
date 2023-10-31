2e0df9dde13b86b1dc385721a2b506755017d763
Dmitriy Martynov <dm346@mail.ru>
2023-10-31 22:21:08 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<template>
+  <div>User {{ $route.params.id }}</div>
+</template>
+
+
+
 <script setup>
 import axios from "axios"
 import {ref} from "vue"
@@ -5,4 +11,19 @@ axios.get("http://localhost:3000/products").then((res)=>{
   console.log(res.data)
 }
 )
+
+
+
+const User = {
+  template: '<div>User</div>'
+}
+
+const router = new VueRouter({
+  routes: [
+    { path: '/user/:id', component: User }
+  ]
+})
+
+
+
 </script>
