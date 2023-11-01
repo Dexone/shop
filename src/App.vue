5ecccebc6d200e54d56f96e8fc2d479dab0bc555
Dmitriy Martynov <dm346@mail.ru>
2023-11-01 11:49:54 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
-  <div>User {{ $route.params.id }}</div>
+  <div v-for="card in cardsInfo"
+    style="background-color: ghostwhite; width: 300px; height: 300px; display: inline-block; margin: 10px; border-radius: 1.5ch">
+    <img style="width: 300px; height: 200px; border-radius: 1.5ch 1.5ch 0ch 0ch;" :src=card.image>
+    {{ card.brand }}{{ card.model }}<br>
+    {{ card.price }}
+  </div>
 </template>
 
 
 
 <script setup>
 import axios from "axios"
-import {ref} from "vue"
-axios.get("http://localhost:3000/products").then((res)=>{
+import { ref } from "vue"
+
+
+
+const cardsInfo = ref([])
+axios.get("http://localhost:3000/products").then((res) => {
   console.log(res.data)
+  const tempData = res.data.map((item, index) => {
+    return {
+      id: res.data[index].id,
+      brand: res.data[index].brand,
+      model: res.data[index].model,
+      owners: res.data[index].owners,
+      year: res.data[index].year,
+      probeg: res.data[index].probeg,
+      transmission: res.data[index].transmission,
+      color: res.data[index].color,
+      price: res.data[index].price,
+      image: res.data[index].image,
+    }
+  })
+  cardsInfo.value = tempData
+  console.log(tempData)
 }
 )
-
-
-
-const User = {
-  template: '<div>User</div>'
-}
-
-const router = new VueRouter({
-  routes: [
-    { path: '/user/:id', component: User }
-  ]
-})
-
-
-
 </script>
---
+++ src/components/info.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-for="card in cardsInfo"
+        style="background-color: ghostwhite; width: 300px; height: 300px; display: inline-block; margin: 10px; border-radius: 1.5ch">
+        <img style="width: 300px; height: 200px; border-radius: 1.5ch 1.5ch 0ch 0ch;" :src=card.image>
+        {{ card.brand }}{{ card.model }}<br>
+        {{ card.price }}
+    </div>
+</template>
+  
+  
+  
+<script setup>
+import axios from "axios"
+import { ref } from "vue"
+
+
+
+const cardsInfo = ref([])
+axios.get("http://localhost:3000/products").then((res) => {
+    weatherData.value = [{
+        id: res.data[index].id,
+        brand: res.data[index].brand,
+        model: res.data[index].model,
+        owners: res.data[index].owners,
+        year: res.data[index].year,
+        probeg: res.data[index].probeg,
+        transmission: res.data[index].transmission,
+        color: res.data[index].color,
+        price: res.data[index].price,
+        image: res.data[index].image,
+    }]
+}
+)
+</script>
+  
