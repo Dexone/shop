5a1f67b3deb98f655c368732fb22da5000851688
Dmitriy Martynov <dm346@mail.ru>
2023-11-06 11:00:51 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-  <router-view>
-  </router-view>
-  <footer>
-{{ carStore.carLength}}
-<RouterLink :to="{ name: 'home'}"><button>1</button></RouterLink>
-<RouterLink :to="{ name: 'page2'}"><button>2</button></RouterLink>
-<RouterLink :to="{ name: 'page3'}"><button>3</button></RouterLink>
-  </footer>
-</div>
-
-
-
+    <router-view>
+    </router-view>
+    <footer>
+      {{ carStore.carLength }}
+      <div style="margin-left: auto; margin-right: auto; width: 200px;">
+        <RouterLink :to="{ name: 'home' }"><button>1</button></RouterLink>
+        <RouterLink :to="{ name: 'page2' }"><button>2</button></RouterLink>
+        <RouterLink :to="{ name: 'page3' }"><button>3</button></RouterLink>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script setup>
