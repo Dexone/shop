b76f0b7b1f6f889ef7e6dae86bf6d5860f1c3e69
Dmitriy Martynov <dm346@mail.ru>
2023-11-08 11:14:08 +0300

    /

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div style="background-color: gainsboro; padding: 10px; border-radius: 10px;">
 
-    <div style="width: 520px; background-color: white; padding: 10px; border-radius: 10px;">
+    <div style="width: 520px; background-color: white; padding: 10px; border-radius: 10px; display: inline-block;">
       <div v-for="card, index in brands" style="display: inline-block;">
         <div @click="filter.push(card)">&nbsp;{{ card }}</div>
       </div>
     </div>
+
+    <div
+      style="width: 520px; background-color: white; padding: 10px; border-radius: 10px; height: 50px; display: inline-block;">
+      <input type="checkbox" value="АКПП"  v-model="filterKuzov" >
+      <label>АКПП</label>
+      <input type="checkbox" value="МКПП" v-model="filterKuzov">
+      <label>МКПП</label>
+      <input type="checkbox" value="Робот" v-model="filterKuzov">
+      <label>Робот</label>
+      <input type="checkbox" value="Вариатор" v-model="filterKuzov">
+      <label>Вариатор</label>
+    </div>
+
+    <div v-for="filKuzov, index in filterKuzov">
+
+    </div>
+
     <div v-for="fil, index in filter" style="display: inline-block;">
       <button style="width: auto; height: 30px; color: red;" @click="filter.splice(index, 1)" class="button">{{ fil }}
         ✖</button>
     </div>
+
     <div>
       <button style="width: auto; height: 30px;border: 0; border-radius: 5px; margin-right: 5px; margin-top: 5px;"
-        @click="filter.length = 0, filterKuzov.length=0">
+        @click="filter.length = 0, filterKuzov.length = 0">
         <RouterLink :to="{ name: 'home' }">Сбросить ✖</RouterLink>
       </button>
       <button style="width: auto; height: 30px; background-color: white; border: 0; border-radius: 5px;">
@@ -21,6 +39,8 @@
       </button>
     </div>
   </div>
+
+  <button @click="console.log(filterKuzov)">test</button>
   <div>
     <router-view>
     </router-view>
@@ -29,6 +49,7 @@
 
     </footer>
   </div>
+
 </template>
 
 <script setup>
